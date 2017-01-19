7a5a812afe3ba4317d0f0b9e076ab2267e11cfc9
Alexei Eraskin <levrun1@gmail.com>
2017-01-19 01:27:20 +0100

    six version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([3,4],{382:function(n,o){},926:function(n,o,c){n.exports=c(382)}},[926]);
+webpackJsonp([3,4],{381:function(n,o){},926:function(n,o,c){n.exports=c(381)}},[926]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
